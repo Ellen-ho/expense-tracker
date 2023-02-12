a0f0bcb31979c17eb00abda91158ae51a3936795
Ellen <ellen31725@gmail.com>
2023-02-13 00:05:21 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
-const routes = require('./routes/index')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
 require('./config/mongoose')
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const UsePassport = require('./config/passport')
 
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers: {
-    ifNotEquals: function (value1, value2) {
-      return value1 !== value2
+const app = express()
+const PORT = process.env.PORT 
+
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      ifEquals: function (value1, value2) {
+        return value1 === value2
+      }
     }
-  }
-}))
+  })
+)
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 // setting static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true 
+}))
+UsePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') 
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
