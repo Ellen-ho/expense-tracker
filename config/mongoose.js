013d349a07f101073a3cdfba7dfc0769472d9712
Ellen <ellen31725@gmail.com>
2023-02-16 17:59:14 +0800

    fix: env import in mongoose config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 // require('mongoose').set('debug', true) => 可以顯示詳細的訊息方便 debug
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
